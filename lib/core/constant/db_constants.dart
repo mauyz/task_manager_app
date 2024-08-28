39e7842da13440ee69c831a0d30f4a58eb657c30
mauyz <tsiorymauyz@gmail.com>
2024-08-28 14:57:19 +0300

    define database constants

---
+++ lib/core/constant/db_constants.dart
@@ -0,0 +1,18 @@
+class DbConstants {
+  const DbConstants._();
+
+  /// Database name
+  static const databaseName = "task_db";
+
+  /// Database version
+  static const databaseVersion = 1;
+
+  /// Task table constants
+  static const tableName = "task_table";
+  static const columnId = "id";
+  static const columnTitle = "title";
+  static const columnDescription = "description";
+  static const columnIsCompleted = "is_completed";
+
+
+}
