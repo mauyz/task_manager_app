bb5f50483fab65b0696d035d4881706d76aa0e31
mauyz <tsiorymauyz@gmail.com>
2024-08-28 13:16:50 +0300

    make task_repository.dart method params final

--- lib/domain/repository/task_repository.dart
+++ lib/domain/repository/task_repository.dart
@@ -2,7 +2,7 @@ import 'package:task_manager_app/domain/model//task.dart';
 
 abstract class  TaskRepository {
   Future<List<Task>> getTaskList();
-  Future<Task> addTask(Task task);
-  Future<void> updateTask(Task task);
-  Future<void> deleteTask(int taskId);
+  Future<Task> addTask(final Task task);
+  Future<void> updateTask(final Task task);
+  Future<void> deleteTask(final int taskId);
 }
