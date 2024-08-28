631da20e16874081ebdf0b63d05cc384ea918b8b
mauyz <tsiorymauyz@gmail.com>
2024-08-28 12:58:52 +0300

    refactor task class

--- lib/domain/entities/task.dart
+++ lib/domain/entities/task.dart
@@ -1,5 +1,5 @@
 class Task {
-  final int? id;
+  final int id;
   final String title;
   final String description;
   final bool isCompleted;
@@ -10,21 +10,6 @@ class Task {
     required this.description,
     required this.isCompleted,
   });
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'title': title,
-      "description"
-          'isCompleted': isCompleted,
-    };
-  }
-
-  Task.fromMap(Map<String, dynamic> map)
-      : id = map['id'] != null ? map['id'] as int : null,
-        title = map['title'] as String,
-        description = map['description'] as String,
-        isCompleted = (map['isCompleted'] as int) == 1 ? true : false;
 
   @override
   int get hashCode => id.hashCode;
