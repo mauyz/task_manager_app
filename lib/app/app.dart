5aef84486f45959a2d6e611e89d61d628ec23541
mauyz <tsiorymauyz@gmail.com>
2024-08-28 15:17:46 +0300

    remove auto_route dependencies

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager_app/app/app_router.dart';
+import 'package:task_manager_app/presentation/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appRouter = AppRouter();
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routerConfig: appRouter.config(),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
-@RoutePage()
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
