463d7bba7f862ae0ff5c37d52b21ea73aadd731a
mauyz <tsiorymauyz@gmail.com>
2024-08-28 14:57:04 +0300

    implement TaskMapper class

---
+++ lib/data/mapper/task_mapper.dart
@@ -0,0 +1,31 @@
+import 'package:task_manager_app/data/entity/task_entity.dart';
+import 'package:task_manager_app/domain/model/task.dart';
+
+class TaskMapper {
+  static Task entityToModel(final TaskEntity map) {
+    return Task(
+      id: map["id"],
+      title: map["title"],
+      description: map["description"],
+      isCompleted: map["isCompleted"] == 1,
+    );
+  }
+
+  static TaskEntity modelToEntity(final Task model) {
+    return {
+      'id': model.id,
+      'title': model.title,
+      'description': model.description,
+      'isCompleted': model.isCompleted ? 1 : 0,
+    };
+  }
+
+  static TaskEntity modelToNewEntity(final Task model) {
+    return {
+      'id': null,
+      'title': model.title,
+      'description': model.description,
+      'isCompleted': model.isCompleted ? 1 : 0,
+    };
+  }
+}
