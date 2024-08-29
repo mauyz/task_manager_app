6937f3c2e49b162bc5501641e0cd6f3c4002716c
mauyz <tsiorymauyz@gmail.com>
2024-08-29 16:20:04 +0300

    implement submit_button.dart

---
+++ lib/presentation/add_or_update/widget/submit_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SubmitButton extends StatelessWidget {
+  final String text;
+  final Function()? onSubmit;
+  const SubmitButton({
+    super.key,
+    required this.text,
+    required this.onSubmit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: onSubmit,
+      child: Text(text),
+    );
+  }
+}
