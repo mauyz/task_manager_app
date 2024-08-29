74126f442b2d1d8d1ccf922748654dae7005e281
mauyz <tsiorymauyz@gmail.com>
2024-08-29 16:19:35 +0300

    implement status form

---
+++ lib/presentation/add_or_update/widget/status_field_form.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StatusFieldForm extends StatelessWidget {
+  final bool selected;
+  final String text;
+  final Function() onSelect;
+  const StatusFieldForm({
+    super.key,
+    required this.selected,
+    required this.text,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      style: selected
+          ? TextButton.styleFrom(
+              iconColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.primary,
+            )
+          : TextButton.styleFrom(
+              iconColor: Theme.of(context).colorScheme.onSecondaryContainer,
+              foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
+            ),
+      onPressed: onSelect,
+      label: Text(text),
+      icon: Icon(
+        selected
+            ? Icons.check_box_rounded
+            : Icons.check_box_outline_blank_rounded,
+      ),
+    );
+  }
+}
