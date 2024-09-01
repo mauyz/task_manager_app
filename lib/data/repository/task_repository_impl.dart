82dc3fe08250ee53088f9fc84cb4470838265510
mauyz <tsiorymauyz@gmail.com>
2024-09-01 13:30:07 +0300

    add comment on data/repository

--- lib/data/repository/task_repository_impl.dart
+++ lib/data/repository/task_repository_impl.dart
@@ -3,11 +3,16 @@ import 'package:task_manager_app/data/mapper/task_mapper.dart';
 import 'package:task_manager_app/domain/model/task.dart';
 import 'package:task_manager_app/domain/repository/task_repository.dart';
 
+/// Implementation of the [TaskRepository] interface that interacts with the data layer.
+///
+/// [TaskRepositoryImpl] acts as a bridge between the domain layer and the data layer,
+/// using the [TaskDatasource] to perform CRUD operations on tasks.
 class TaskRepositoryImpl implements TaskRepository {
   final TaskDatasource taskDatasource;
 
   TaskRepositoryImpl(this.taskDatasource);
 
+  /// Retrieves a list of all tasks from the data source.
   @override
   Future<List<Task>> getTaskList() async {
     final entityList = await taskDatasource.getAllTasks();
@@ -18,6 +23,7 @@ class TaskRepositoryImpl implements TaskRepository {
         .toList();
   }
 
+  /// Adds a new task to the data source.
   @override
   Future<Task> addTask(Task task) async {
     final entity = await taskDatasource.insertTask(
@@ -26,11 +32,13 @@ class TaskRepositoryImpl implements TaskRepository {
     return TaskMapper.entityToModel(entity);
   }
 
+  /// Updates an existing task in the data source.
   @override
   Future<void> updateTask(Task task) async {
     await taskDatasource.updateTask(TaskMapper.modelToEntity(task));
   }
 
+  /// Deletes a task from the data source by its ID.
   @override
   Future<void> deleteTask(int taskId) async {
     await taskDatasource.deleteTask(taskId);
--- lib/data/repository/task_repository_provider.dart
+++ lib/data/repository/task_repository_provider.dart
@@ -5,6 +5,7 @@ import 'package:task_manager_app/domain/repository/task_repository.dart';
 
 part 'task_repository_provider.g.dart';
 
+/// A Riverpod provider for the [TaskRepository] that supplies the implementation.
 @riverpod
 TaskRepository taskRepository(TaskRepositoryRef ref) {
     final datasource = ref.read(taskDatabaseProvider);
