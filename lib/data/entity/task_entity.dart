f2de59ec178274ab943fc922e5ef61e8f4ebdc8d
mauyz <tsiorymauyz@gmail.com>
2024-09-01 13:29:45 +0300

    add comment on task_entity.dart

--- lib/data/entity/task_entity.dart
+++ lib/data/entity/task_entity.dart
@@ -1,2 +1,5 @@
+/// A type alias for a task entity represented as a map.
 typedef TaskEntity = Map<String, dynamic>;
+
+/// A type alias for a list of task entities.
 typedef TaskListEntity = List<TaskEntity>;
