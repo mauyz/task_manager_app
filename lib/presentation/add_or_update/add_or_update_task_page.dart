3989f7c195b75968ba50175492f1b043296728a5
mauyz <tsiorymauyz@gmail.com>
2024-09-01 14:04:33 +0300

    add comment on add_or_update_task_page.dart

--- lib/presentation/add_or_update/add_or_update_task_page.dart
+++ lib/presentation/add_or_update/add_or_update_task_page.dart
@@ -3,6 +3,10 @@ import 'package:task_manager_app/domain/model/task.dart';
 import 'package:task_manager_app/presentation/add_or_update/layout/operation_form_layout.dart';
 import 'package:task_manager_app/presentation/home/widget/simple_app_bar.dart';
 
+/// A page for adding a new task or updating an existing task.
+///
+/// - If no [Task] is provided (i.e., `task` is `null`), the page is used to add a new task.
+/// - If a [Task] is provided, the page is used to update that task.
 class AddOrUpdateTaskPage extends StatelessWidget {
   final Task? task;
   const AddOrUpdateTaskPage({
