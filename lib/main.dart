efb08672583d86d12a3f5d2ccb7a9139dcb9978d
mauyz <tsiorymauyz@gmail.com>
2024-09-01 13:12:05 +0300

    add comment on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:task_manager_app/app/app.dart';
 
 void main() {
+  // The entry point of the Flutter application.
+  // This sets up the application with Riverpod's ProviderScope,
+  // which makes providers accessible throughout the widget tree.
   runApp(
     const ProviderScope(
       child: App(),
