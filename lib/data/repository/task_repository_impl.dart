903204660ec1312a0b61d9d98aed98c404c13799
mauyz <tsiorymauyz@gmail.com>
2024-09-16 15:08:11 +0300

    not use async on TaskRepository implementation

--- lib/data/repository/task_repository_impl.dart
+++ lib/data/repository/task_repository_impl.dart
@@ -14,33 +14,31 @@ class TaskRepositoryImpl implements TaskRepository {
 
   /// Retrieves a list of all tasks from the data source.
   @override
-  Future<List<Task>> getTaskList() async {
-    final entityList = await taskDatasource.getAllTasks();
-    return entityList
-        .map(
-          (e) => TaskMapper.entityToModel(e),
-        )
-        .toList();
+  Future<List<Task>> getTaskList() {
+    return taskDatasource.getAllTasks().then(
+      (list) {
+        return list.map((e) => TaskMapper.entityToModel(e)).toList();
+      },
+    );
   }
 
   /// Adds a new task to the data source.
   @override
-  Future<Task> addTask(Task task) async {
-    final entity = await taskDatasource.insertTask(
-      TaskMapper.modelToNewEntity(task),
-    );
-    return TaskMapper.entityToModel(entity);
+  Future<Task> addTask(Task task) {
+    return taskDatasource
+        .insertTask(TaskMapper.modelToNewEntity(task))
+        .then(TaskMapper.entityToModel);
   }
 
   /// Updates an existing task in the data source.
   @override
-  Future<void> updateTask(Task task) async {
-    await taskDatasource.updateTask(TaskMapper.modelToEntity(task));
+  Future<void> updateTask(Task task) {
+    return taskDatasource.updateTask(TaskMapper.modelToEntity(task));
   }
 
   /// Deletes a task from the data source by its ID.
   @override
-  Future<void> deleteTask(int taskId) async {
-    await taskDatasource.deleteTask(taskId);
+  Future<void> deleteTask(int taskId) {
+    return taskDatasource.deleteTask(taskId);
   }
 }
--- test/unit_test/task_repository_test.dart
+++ test/unit_test/task_repository_test.dart
@@ -10,9 +10,9 @@ void main() {
   final taskRepository = TaskRepositoryImpl(taskDatasource);
   group("TaskRepository", () {
     setUp(
-      () async {
+      () {
         when(taskDatasource.getAllTasks()).thenAnswer(
-          (_) async => [
+          (_) => Future.value([
             {
               "id": 1,
               "title": "title 1",
@@ -25,17 +25,20 @@ void main() {
               "description": "description 1",
               "is_completed": 0,
             }
-          ],
+          ]),
         );
       },
     );
 
-    test("getAllTasks should return the entity list", () async {
-      final list = await taskRepository.getTaskList();
-      expect(list.length, 2);
-      expect(list.first.id, 1);
-      expect(list.first.isCompleted, true);
-      verify(taskDatasource.getAllTasks()).called(1);
+    test("getAllTasks should return the entity list", () {
+      taskRepository.getTaskList().then(
+        (list) {
+          expect(list.length, 2);
+          expect(list.first.id, 1);
+          expect(list.first.isCompleted, true);
+          verify(taskDatasource.getAllTasks()).called(1);
+        },
+      );
     });
   });
 
@@ -47,29 +50,32 @@ void main() {
       "is_completed": 0,
     };
 
-    setUp(() async {
+    setUp(() {
       when(taskDatasource.insertTask(testEntity)).thenAnswer(
-        (_) async => {
+        (_) => Future.value({
           "id": 10,
           "title": "title 1",
           "description": "description 1",
           "is_completed": 0,
-        },
+        }),
       );
     });
 
     test(
       "insertTask should return the inserted task",
-      () async {
+      () {
         final newTask = Task(
           id: 0,
           title: "title 1",
           description: "description 1",
           isCompleted: false,
         );
-        final result = await taskRepository.addTask(newTask);
-        expect(result.id, 10);
-        verify(taskDatasource.insertTask(testEntity)).called(1);
+        taskRepository.addTask(newTask).then(
+          (result) {
+            expect(result.id, 10);
+            verify(taskDatasource.insertTask(testEntity)).called(1);
+          },
+        );
       },
     );
   });
@@ -82,40 +88,45 @@ void main() {
       "is_completed": 1,
     };
 
-    setUp(() async {
+    setUp(() {
       when(taskDatasource.updateTask(testEntity)).thenAnswer(
-        (_) async => {},
+        (_) => Future.value(),
       );
     });
 
     test(
       "updateTask should return void",
-      () async {
-        await taskRepository.updateTask(
-          Task(
-            id: 1,
-            title: "new title",
-            description: "new description",
-            isCompleted: true,
-          ),
-        );
-        verify(taskDatasource.updateTask(testEntity)).called(1);
+      () {
+        taskRepository
+            .updateTask(
+              Task(
+                id: 1,
+                title: "new title",
+                description: "new description",
+                isCompleted: true,
+              ),
+            )
+            .then(
+              (value) =>
+                  verify(taskDatasource.updateTask(testEntity)).called(1),
+            );
       },
     );
   });
 
   group("TaskRepository", () {
-    setUp(() async {
+    setUp(() {
       when(taskDatasource.deleteTask(1)).thenAnswer(
-        (_) async => {},
+        (_) => Future.value(),
       );
     });
 
     test(
       "deleteTask should return void",
-      () async {
-        await taskRepository.deleteTask(1);
-        verify(taskDatasource.deleteTask(1)).called(1);
+      () {
+        taskRepository.deleteTask(1).then(
+              (value) => verify(taskDatasource.deleteTask(1)).called(1),
+            );
       },
     );
   });
